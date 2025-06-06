cdf2aa9f09c48841809dbb21ce8391dfb6877b84
Ben Zhang <benzhangniu@gmail.com>
2025-06-05 21:17:39 -0700

    Add end effector camera

--- lerobot/common/robot_devices/robots/configs.py
+++ lerobot/common/robot_devices/robots/configs.py
@@ -488,6 +488,12 @@ class So101RobotConfig(ManipulatorRobotConfig):
                 width=640,
                 height=480,
             ),
+            "endeffector": OpenCVCameraConfig(
+                camera_index=2,
+                fps=30,
+                width=640,
+                height=480,
+            ),
         }
     )
 

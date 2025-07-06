51cb9e2af914957f75608e32454606d5865cac1a
Ben Zhang <benzhangniu@gmail.com>
2025-07-05 21:13:41 -0700

    Change config to perform smolvla training on RTX 6000 Ada

--- slurm/watgpu.sh
+++ slurm/watgpu.sh
@@ -22,34 +22,37 @@ DATASET_NAME="so101_die_mat1"
 DATASET_REPO_ID=${HF_USER}/${DATASET_NAME}
 
 # act
-BATCH_SIZE=64
-LR=5e-5
-POLICY_REPO_ID="${HF_USER}/act_${DATASET_NAME}_b${BATCH_SIZE}"
+# BATCH_SIZE=64
+# LR=5e-5
+# POLICY_REPO_ID="${HF_USER}/act_${DATASET_NAME}_b${BATCH_SIZE}"
+# python -m lerobot.scripts.train \
+#   --dataset.repo_id=${DATASET_REPO_ID} \
+#   --policy.type=act \
+#   --policy.repo_id=${POLICY_REPO_ID} \
+#   --output_dir=../outputs/train/${POLICY_REPO_ID} \
+#   --job_name=${POLICY_REPO_ID}_${CLUSTER_NAME} \
+#   --policy.device=cuda \
+#   --optimizer.lr=${LR} \
+#   --wandb.enable=true \
+#   --num_workers=8 \
+#   --batch_size=${BATCH_SIZE} \
+#   --steps=800_000 \
+#   --save_freq=5_000
+
+# smolvla
+BATCH_SIZE=128
+LR=5e-4
+POLICY_REPO_ID="${HF_USER}/smolvla_${DATASET_NAME}_b${BATCH_SIZE}"
 python -m lerobot.scripts.train \
   --dataset.repo_id=${DATASET_REPO_ID} \
-  --policy.type=act \
+  --policy.path=lerobot/smolvla_base \
   --policy.repo_id=${POLICY_REPO_ID} \
   --output_dir=../outputs/train/${POLICY_REPO_ID} \
   --job_name=${POLICY_REPO_ID}_${CLUSTER_NAME} \
   --policy.device=cuda \
-  --optimizer.lr=${LR} \
   --wandb.enable=true \
   --num_workers=8 \
   --batch_size=${BATCH_SIZE} \
+  --optimizer.lr=${LR} \
   --steps=800_000 \
   --save_freq=5_000
-
-# # smolvla
-# EXP_SUFFIX="_${CLUSTER_NAME}_h200_b256"
-# python -m lerobot.scripts.train \
-#   --dataset.repo_id=${HF_USER}/so101_eraser_mat1 \
-#   --policy.path=lerobot/smolvla_base \
-#   --policy.repo_id=${HF_USER}/smolvla_so101_eraser_mat1 \
-#   --output_dir=../outputs/train/smolvla_so101_eraser_mat1${EXP_SUFFIX} \
-#   --job_name=smolvla_so101_eraser_mat1${EXP_SUFFIX} \
-#   --policy.device=cuda \
-#   --wandb.enable=true \
-#   --num_workers=8 \
-#   --batch_size=256 \
-#   --steps=800_000 \
-#   --save_freq=5_000

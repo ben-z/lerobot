8542253a9909534dc964482fafafa5f3bc9f4cd0
Ben Zhang <benzhangniu@gmail.com>
2025-07-06 12:08:39 -0700

    Set LR properly, and train ACT at b128 lr1e-4

--- slurm/watgpu.sh
+++ slurm/watgpu.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=chunksize1s
+#SBATCH --job-name=robo
 # watgpu108: RTX 6000 Ada x8 (49140MiB ~48GiB)
 # watgpu208: RTX 6000 Ada x8 (49140MiB ~48GiB)
 # watgpu308: L40S x4 (46068MiB ~45GiB), RTX A6000 x2 (49140MiB ~48GiB), RTX 6000 Ada x2 (49140MiB ~48GiB)
 # watgpu408: L40S x8 (46068MiB ~45GiB)
-# watgpu508: H200 x4? (143771MiB ~140GiB)
-#SBATCH --nodelist=watgpu308
+# watgpu502: H200 x2? (x6 in lspci) (143771MiB ~140GiB)
+# watgpu508: H200 x4? (x6 in lspci) (143771MiB ~140GiB)
+#SBATCH --nodelist=watgpu502
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=256G
@@ -27,10 +28,10 @@ DATASET_REPO_ID="${HF_USER}/${DATASET_NAME}"
 # ACT
 # Tuning tips: https://github.com/tonyzhaozh/act/blob/742c753c0d4a5d87076c8f69e5628c79a8cc5488/README.md#new-act-tuning-tips
 # 64 uses ~47GiB VRAM, 128 uses ~93GiB VRAM
-BATCH_SIZE=64
-LR=5e-5
-POLICY_REPO_ID="${HF_USER}/act_${DATASET_NAME}_b${BATCH_SIZE}_${SLURM_JOB_NAME}"
-WANDB_NOTES="chunk_size=30, n_action_steps=30"
+BATCH_SIZE=128
+LR=1e-4
+POLICY_REPO_ID="${HF_USER}/act_${DATASET_NAME}_b${BATCH_SIZE}_lr${LR}_${SLURM_JOB_NAME}"
+WANDB_NOTES="batch_size=${BATCH_SIZE}, lr=${LR}"
 python -m lerobot.scripts.train \
   --dataset.repo_id="${DATASET_REPO_ID}" \
   --policy.type=act \
@@ -38,9 +39,7 @@ python -m lerobot.scripts.train \
   --output_dir="../outputs/train/${POLICY_REPO_ID}" \
   --job_name="${POLICY_REPO_ID}_${CLUSTER_NAME}" \
   --policy.device=cuda \
-  --policy.chunk_size=30 \
-  --policy.n_action_steps=30 \
-  --optimizer.lr="${LR}" \
+  --policy.optimizer_lr="${LR}" \
   --wandb.enable=true \
   --wandb.notes="${WANDB_NOTES}" \
   --num_workers=8 \
@@ -63,6 +62,6 @@ python -m lerobot.scripts.train \
 #   --wandb.enable=true \
 #   --num_workers=8 \
 #   --batch_size="${BATCH_SIZE}" \
-#   --optimizer.lr="${LR}" \
+#   --policy.optimizer_lr="${LR}" \
 #   --steps="800_000" \
 #   --save_freq="5_000"
